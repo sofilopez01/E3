1657b500331fb83c5203c44f41a3c6a6e10475c5
slopez <sofiabelen648@gmail.com>
2024-04-02 17:59:05 -0300

    inicio

---
+++ index.js
@@ -0,0 +1,86 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+    imagen: "./img/muzzarella.png",
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+    imagen: "./img/cebolla.png",
+  },
+
+  {
+    id: 3,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+    imagen: "./img/4quesos.png",
+  },
+
+  {
+    id: 4,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
+    imagen: "./img/especial.png",
+  },
+
+  {
+    id: 5,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+    imagen: "./img/anana.png",
+  },
+];
+
+const buscarBtn = document.getElementById('buscarBtn');
+const resultado = document.getElementById('resultado');
+let ultimaPizzaBuscada = JSON.parse(localStorage.getItem('ultimaPizza'));
+
+function renderizarPizza(pizza) {
+    resultado.innerHTML = `
+        <div class="card">
+            <img src="${pizza.imagen}" alt="${pizza.nombre}">
+            <h3>${pizza.nombre}</h3>
+            <p>Precio: $${pizza.precio}</p>
+        </div>
+    `;
+}
+
+function guardarUltimaPizza(pizza) {
+    localStorage.setItem('ultimaPizza', JSON.stringify(pizza));
+}
+
+function buscarPizza() {
+    const inputNumero = document.getElementById('pizzaId').value;
+    const pizzaEncontrada = pizzas.find(pizza => pizza.id === parseInt(inputNumero));
+
+    if (!inputNumero) {
+        resultado.innerHTML = "<p class='error'>Por favor ingrese un número válido.</p>";
+    } else if (pizzaEncontrada) {
+        renderizarPizza(pizzaEncontrada);
+        guardarUltimaPizza(pizzaEncontrada);
+    } else {
+        resultado.innerHTML = "<p class='error'>No se encontró ninguna pizza con ese ID.</p>";
+    }
+}
+
+buscarBtn.addEventListener('click', buscarPizza);
+
+// Mostrar la última pizza buscada al recargar la página
+if (ultimaPizzaBuscada) {
+    renderizarPizza(ultimaPizzaBuscada);
+}
